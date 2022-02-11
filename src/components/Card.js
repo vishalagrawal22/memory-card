cd42765fc7662a3f3f5f49f300241f643eeb049b
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-02-11 16:44:52 +0530

    Add card item

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,18 @@
 import uniqid from 'uniqid';
+import '../styles/Card.css';
 
 function CardFactory(title, imageSrc) {
   const id = uniqid('card-');
   return { id, title, imageSrc };
 }
 
-export { CardFactory };
+function CardItem({ card, onCardClick }) {
+  return (
+    <li key={card.id} id={card.id} onClick={onCardClick}>
+      <img src={card.imageSrc} alt={card.title} />
+      <h3>{card.title}</h3>
+    </li>
+  );
+}
+
+export { CardFactory, CardItem };
