ebb7b4922b2e35a8beabd9eb900c3945f0f17413
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-02-12 11:59:19 +0530

    Add temporary UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
+import { useState, useEffect } from 'react';
+import { PokemonCardList } from './components/PokemonCardList';
+
 function App() {
-  return <div></div>;
+  const [currentScore, setCurrentScore] = useState(0);
+  const [maxScore, setMaxScore] = useState(0);
+  const [gameStatus, setGameStatus] = useState('ongoing');
+
+  function onValidClick() {
+    setCurrentScore((prevScore) => prevScore + 1);
+  }
+
+  function onInvalidClick() {
+    setGameStatus('game over');
+  }
+
+  function onFinish() {
+    setGameStatus('game finished');
+  }
+
+  useEffect(() => {
+    setMaxScore((maxScore) => Math.max(maxScore, currentScore));
+  }, [currentScore]);
+
+  if (gameStatus === 'ongoing') {
+    return (
+      <PokemonCardList
+        numberOfCards={12}
+        gameNumber={1}
+        onValidClick={onValidClick}
+        onInvalidClick={onInvalidClick}
+        onFinish={onFinish}
+      ></PokemonCardList>
+    );
+  } else {
+    return (
+      <div>
+        Game Status: {gameStatus}
+        <br />
+        Current Score: {currentScore}
+        <br />
+        Max Score: {maxScore}
+      </div>
+    );
+  }
 }
 
 export default App;
