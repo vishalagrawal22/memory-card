8c3470fac90550c413f1743e7beffe0aed8ed99e
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-02-12 16:20:19 +0530

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import '../styles/Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        Data API: <a href="https://pokeapi.co/">Poke API</a>
+      </p>
+    </footer>
+  );
+}
+
+export { Footer };
